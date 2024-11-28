aee6b8938bd0e21c417aedd6f34a0bd9621caf5a
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-28 13:22:16 +0300

    CorrectRec.py

--- collision/CorrectRec.py
+++ collision/CorrectRec.py
@@ -1,1 +1,12 @@
+class RectCorrectError(Exception):
+    pass
 
+def isCorrectRect(rectangles):
+    counter = 0
+    
+    for i in rectangles:
+        counter += 1
+        if i[0][0] >= i[1][0] or i[0][1] >= i[1][1]:
+            raise ValueError(f'Один из прямоугольников некорректный номер {counter}')
+
+    return True
